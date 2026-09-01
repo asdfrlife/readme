bcca79a6721e212decfc977b393c39910dac95ad
asdfrlife <luckytruman359@gmail.com>
2026-09-01 22:35:56 +0300

    canvas ressizing

--- app/(main)/view/page.tsx
+++ app/(main)/view/page.tsx
@@ -60,8 +60,10 @@ export default async function ViewPage(props: {
         <div className="w-24" /> {/* Spacer for centering title */}
       </div>
 
-      <div className="flex-1 w-full overflow-hidden">
-        <EpubViewer url={signedUrlData.signedUrl} />
+      <div className="flex-1 w-full overflow-hidden flex justify-center">
+        <div className="h-full aspect-[1/1.414] max-w-full">
+          <EpubViewer url={signedUrlData.signedUrl} />
+        </div>
       </div>
     </div>
   )
